eabf5a93e0ab62a064d6a140cfc3360861b09759
KevinGrajeda <me@kvin.me>
2024-09-20 17:54:18 -0600

    add example page

---
+++ web/tailwind.config.mjs
@@ -0,0 +1,10 @@
+import tailwindMotion from "../src/index.js";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [tailwindMotion],
+};
